42095c3a7e7094cd8582cf97d44ad3a90300d219
Emeka Victor Mbachu <victor.c.mbachu@gmail.com>
2021-12-11 14:22:29 +0100

    update

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,73 +1,94 @@
-@extends('layouts.main')
+<!DOCTYPE html>
+<html lang="eng" dir="ltr">
 
-@section('title')
-    Admin Login
-@endsection
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8">
+    <meta name="description" content="Cryptcoin Investment and Tradings">
+    <meta name="keywords" content="Cryptcoin Investment and Tradings">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#fc5b3f" />
+    <!-- Site Properties -->
+    <title>Admin Login - Cryptcoin Investment and Tradings</title>
+    <!-- Critical preload -->
+    <link rel="preload" href="{{ asset('main/js/vendors/uikit.min.js') }}" as="script">
+    <link rel="preload" href="{{ asset('main/css/vendors/uikit.min.css') }}" as="style">
+    <link rel="preload" href="{{ asset('main/css/style.css') }}" as="style">
+    <!-- Icon preload -->
+    <link rel="preload" href="{{ asset('main/fonts/fa-brands-400.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('main/fonts/fa-solid-900.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <!-- Font preload -->
+    <link rel="preload" href="{{ asset('main/fonts/inter-v2-latin-regular.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('main/fonts/inter-v2-latin-500.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('main/fonts/inter-v2-latin-700.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <!-- Favicon and apple icon -->
+    <link rel="shortcut icon" href="{{ asset('logoonly.png') }}" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('logoonly.png') }}">
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('main/css/vendors/uikit.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('main/css/style.css') }}">
 
-@section('content')
-    <div class="corzo-page-title-wrap corzo-style-custom corzo-left-align">
-        <div class="corzo-header-transparent-substitute"></div>
-        <div class="corzo-page-title-overlay"></div>
-        <div class="corzo-page-title-container corzo-container">
-            <div class="corzo-page-title-content corzo-item-pdlr">
-                <h1 class="corzo-page-title">Admin Login</h1>
-                <div class="corzo-page-caption-divider"></div>
-            </div>
-        </div>
-    </div>
+    <script src="{{ asset('js/countries.js') }}" type="text/javascript"></script>
+</head>
 
-    <div class="gdlr-core-pbf-wrapper" style="padding: 10px 0px 35px 0px;">
-        <div class="gdlr-core-pbf-background-wrap" style="background-color: #ffffff;"></div>
-        <div class="gdlr-core-pbf-wrapper-content gdlr-core-js">
-            <div class="gdlr-core-pbf-wrapper-container clearfix gdlr-core-container">
-                <div class="gdlr-core-pbf-column gdlr-core-column-60 gdlr-core-column-first">
-                    <div class="gdlr-core-pbf-column-content-margin gdlr-core-js">
-                        <div class="gdlr-core-pbf-background-wrap"></div>
-                        <div class="gdlr-core-pbf-column-content clearfix gdlr-core-js" style="max-width: 760px;">
-                            <div class="gdlr-core-pbf-element">
-                                <div class="gdlr-core-title-item gdlr-core-item-pdb clearfix gdlr-core-center-align gdlr-core-title-item-caption-bottom gdlr-core-item-pdlr" style="padding-bottom: 60px;">
-                                    <div class="gdlr-core-title-item-title-wrap">
-                                        <h3 class="gdlr-core-title-item-title gdlr-core-skin-title"
-                                            style="font-size: 39px; font-weight: 600; letter-spacing: 0px; text-transform: none;">
-                                            Login as an admin <span class="gdlr-core-title-item-title-divider gdlr-core-skin-divider"></span>
-                                        </h3>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div class="gdlr-core-pbf-element">
-                                <div class="gdlr-core-contact-form-7-item gdlr-core-item-pdlr gdlr-core-item-pdb">
-                                    <div role="form" class="wpcf7" id="wpcf7-f1979-p1964-o1" dir="ltr" lang="en-US">
-                                        <div class="screen-reader-response">
-                                            <p role="status" aria-live="polite" aria-atomic="true"></p>
-                                            <ul></ul>
-                                        </div>
-                                        @include('includes.alerts')
-                                    <form method="POST" action="{{ route('admin-login') }}" enctype="multipart/form-data">
-                                        @csrf
-                                            <input type="text" name="username" id="Email"
-                                                   placeholder="Username:"
-                                                   class="input @error('username') is-invalid @enderror">
-                                            @error('username')
-                                            <span class="invalid-feedback" role="alert">
+<body>
+<!-- preloader begin -->
+<div class="in-loader">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+<!-- preloader end -->
+<main>
+    <!-- section content begin -->
+    <div class="uk-section uk-padding-remove-vertical">
+        <div class="uk-container uk-container-expand">
+            <div class="uk-grid" data-uk-height-viewport="expand: true">
+                <div class="uk-width-2-5@m uk-background-cover uk-background-center-right uk-visible@m uk-box-shadow-xlarge" style="background-image: url({{ asset('main/img/in-signin-image.jpg') }});">
+                </div>
+                <div class="uk-width-expand@m uk-flex uk-flex-middle">
+                    <div class="uk-grid uk-flex-center">
+                        <div class="uk-width-3-5@m">
+                            <div class="in-padding-horizontal@s">
+                                <!-- logo begin -->
+                                <a class="uk-logo" href="{{ url('/') }}">
+                                    <img src="{{ asset('logo.png') }}" data-src="{{ asset('logo.png') }}"
+                                         alt="logo" width="160" height="34" data-uk-img>
+                                </a>
+                                <!-- logo end -->
+                                <p class="uk-text-lead uk-margin-top uk-margin-remove-bottom">Log into your account</p>
+                                <!-- login form begin -->
+                                <form class="uk-grid uk-form" action="{{ route('admin-login') }}" method="post">
+                                    @csrf
+                                    <label>Email</label>
+                                    <div class="uk-margin-small uk-width-1-1 uk-inline">
+                                        <span class="uk-form-icon uk-form-icon-flip fas fa-user fa-sm"></span>
+                                        <input class="@error('username') is-invalid @enderror uk-input uk-border-rounded"
+                                               id="username" name="username" type="text" placeholder="Username" required>
+                                        @error('username')
+                                        <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
-                                            @enderror
-
-                                            <input type="password" name="password" id="Name"
-                                                   placeholder="Password:"
-                                                   class="input @error('password') is-invalid @enderror">
-                                            @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-
-                                            <input type="submit" name="submit" value="Login"
-                                                   class="submit-button">
-                                        </form>
+                                        @enderror
                                     </div>
-                                </div>
+
+                                    <label>Password</label>
+                                    <div class="uk-margin-small uk-width-1-1 uk-inline">
+                                        <span class="uk-form-icon uk-form-icon-flip fas fa-lock fa-sm"></span>
+                                        <input class="@error('password') is-invalid @enderror @error('password') is-invalid @enderror uk-input uk-border-rounded"
+                                               type="password" name="password" placeholder="Password"
+                                               autocomplete="new-password" required>
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                        @enderror
+                                    </div>
+                                    <div class="uk-margin-small uk-width-1-1">
+                                        <button class="uk-button uk-width-1-1 uk-button-primary uk-border-rounded uk-float-left" type="submit" name="submit">Sign in</button>
+                                    </div>
+                                </form>
+                                <!-- login form end -->
                             </div>
                         </div>
                     </div>
@@ -75,4 +96,12 @@
             </div>
         </div>
     </div>
-@endsection
+    <!-- section content end -->
+</main>
+<!-- Javascript -->
+<script src="{{ asset('main/js/vendors/uikit.min.js') }}"></script>
+<script src="{{ asset('main/js/vendors/blockit.min.js') }}"></script>
+<script src="{{ asset('main/js/config-theme.js') }}"></script>
+</body>
+
+</html>
